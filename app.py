6b040fa3530c7476a3296806fa43733eda966650
Adam Smith <adam.smith@greermiddlecollege.org>
2020-10-26 12:13:03 -0400

    Setting up flask project to serve Pixel Perfect

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    index_html = open("index.html")
+    return index_html.read()
+
+
+if __name__ == '__main__':
+    port = 5000
+    app.run(host='0.0.0.0', port=port)
