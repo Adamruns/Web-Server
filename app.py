d42d87893d5fb0674834b094b5b94468b3893c09
Adam Smith <adam.smith@greermiddlecollege.org>
2020-11-19 21:25:55 -0500

    Tried to get csv file contents to show with no luck

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask
+import csv
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    index_html = open("index.html")
-    return index_html.read()
+    with open("index.html") as index_html:
+        return index_html.read().replace("{{paragraphs}}", str(csvfile))
 
 
-# if __name__ == '__main__':
-#     port = 5000
-#     app.run(host='0.0.0.0', port=port)
+@app.route('/about')
+def about():
+    return "About"
+
+
+with open('static/text.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+if __name__ == '__main__':
+    port = 5000
+    app.run(host='0.0.0.0', port=port)
